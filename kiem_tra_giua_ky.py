2104935681ad76f366a9a9867108485714210fd1
Makee13 <wilsonkylerkl@gmail.com>
2022-10-14 07:50:56 +0700

    3

--- kiem_tra_giua_ky.py
+++ kiem_tra_giua_ky.py
@@ -3,7 +3,10 @@ import pandas
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.ensemble import BaggingRegressor
+import warnings
+warnings.filterwarnings("ignore")
 
+# Bài 1:
 data = pandas.read_csv('data_Ktra2.csv')
 X = data[['X1', 'X2', 'X3', 'X4', 'X5', 'X6']]
 # Remove NaN
@@ -12,8 +15,9 @@ y = data[['Y1']]
 # Remove NaN
 y = y.dropna()
 # Tách dữ liệu thành 2 phần
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = None, test_size = 0.3)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.3)
+# Bài 2
 # Xây dựng mô hình hồi quy để dự đoán biến Y1
 # Building Xbar
 one = np.ones((X_train.shape[0], 1))
@@ -30,27 +34,50 @@ w_3 = w[3][0]
 w_4 = w[4][0]
 w_5 = w[5][0]
 w_6 = w[6][0]
-X1 = 0.64
-X2 = 784
-X3 = 343
-X4 = 220.5
-X5 = 3.5
-X6 = 2
-y1 = w_0 + w_1*X1 + w_2*X2 + w_3*X3 + w_4*X4 + w_5*X5 + w_6*X6
+X1_test = np.asarray(X_test['X1'])
+X2_test = np.asarray(X_test['X2'])
+X3_test = np.asarray(X_test['X3'])
+X4_test = np.asarray(X_test['X4'])
+X5_test = np.asarray(X_test['X5'])
+X6_test = np.asarray(X_test['X6'])
+y_test = np.asarray(y_test)
 # print( 'Solution found by (5): ', w)
-print(y1)
-
+# Dự đoán biến Y1
+y1 = []
+for i in range(X_test.shape[0]):
+    y1_pred = w_0 + w_1*X1_test[i] + w_2*X2_test[i] + w_3*X3_test[i] + w_4*X4_test[i] + w_5*X5_test[i] + w_6*X6_test[i]
+    y1.append(y1_pred)
+# print(y1)
 # Sử dụng thư viện
 # fit the model by Linear Regression
 regr = linear_model.LinearRegression(fit_intercept=False) # fit_intercept = False for calculating the bias
 regr.fit(X_train, y_train)
-y_pred1 = regr.predict([[X1,X2,X3,X4,X5,X6]])
-print(y_pred1)
+y_pred_train = regr.predict(X_train)
+y_pred_val = regr.predict(X_validation)
+y_pred_test = regr.predict(X_test)
+
+# (RMSE) Đánh giá mô hình
+diff_train = np.sqrt(np.sum(y_train-y_pred_train)**2)/y_train.shape[0]
+diff_val = np.sqrt(np.sum(y_validation-y_pred_val)**2)/y_validation.shape[0]
+diff_test = np.sqrt(np.sum(y_test-y_pred_test)**2)/y_test.shape[0]
+print(diff_train, diff_val, diff_test)
 # print( 'Solution found by scikit-learn  : ', regr.coef_)
 
 # Bài 4:
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import BaggingRegressor
-regr = BaggingRegressor(base_estimator=LinearRegression(), n_estimators=30, random_state=0).fit(X_train, y_train.values.ravel())
-y_pred2 = regr.predict([[X1,X2,X3,X4,X5,X6]])
-print(y_pred2.T)
+bagging_regr = BaggingRegressor(base_estimator=LinearRegression(), n_estimators=30, random_state=0).fit(X_train, y_train.values.ravel())
+def baggingPredict(X):
+    result = []
+    for i in range(X.shape[0]):
+        result.append(bagging_regr.predict([X.iloc[i]]))
+    return np.asarray(result)
+y_pred_train = baggingPredict(X_train)
+y_pred_val = baggingPredict(X_validation)
+y_pred_test = baggingPredict(X_test)
+
+# (RMSE) Đánh giá mô hình
+diff_train = np.sqrt(np.sum(y_train-y_pred_train)**2)/y_train.shape[0]
+diff_val = np.sqrt(np.sum(y_validation-y_pred_val)**2)/y_validation.shape[0]
+diff_test = np.sqrt(np.sum(y_test-y_pred_test)**2)/y_test.shape[0]
+print(diff_train, diff_val, diff_test)
